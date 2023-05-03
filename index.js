d453292c866d919aaa79f07b7ab2654687424b32
serteri <serteri@hotmail.com>
2023-05-03 22:34:17 +1000

    Four digit number generat ed

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 let x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let randomChoose = x[Math.floor(Math.random() * 9 + 1)]
+let m = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let randomChoose = [x[Math.floor(Math.random() * 8 + 1)]]
 console.log(randomChoose)
+let k = new Set(randomChoose);
+
+
+
+while (k.size != 4) {
+    k.add(m[Math.floor(Math.random() * 9 + 1)])
+
+}
+
+
+
+let fourDigit = [...k].join('')
+
+console.log(fourDigit)
