b80f677cd2aa524c96a1fb87ac6857c74d28c037
serteri <serteri@hotmail.com>
2023-06-04 23:16:59 +1000

    utton functionining

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-let x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let m = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let randomChoose = [x[Math.floor(Math.random() * 8 + 1)]]
-console.log(randomChoose)
-let k = new Set(randomChoose);
+let number = document.querySelector('.btn')
+
+number.addEventListener('click', numberGenerate)
+
+function numberGenerate() {
+    let x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let m = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let randomChoose = [x[Math.floor(Math.random() * 8 + 1)]]
+    console.log(randomChoose)
+    let k = new Set(randomChoose);
 
 
 
-while (k.size != 4) {
-    k.add(m[Math.floor(Math.random() * 9 + 1)])
+    while (k.size != 4) {
+        k.add(m[Math.floor(Math.random() * 9 + 1)])
 
+    }
+
+
+
+    let fourDigit = [...k].join('')
+
+    console.log(fourDigit)
 }
-
-
-
-let fourDigit = [...k].join('')
-
-console.log(fourDigit)
